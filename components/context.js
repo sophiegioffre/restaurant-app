d0feb05344ad3314c87527ec22d5070681a34de8
Sophie <sophie.gioff@gmail.com>
2022-09-10 20:12:45 -0400

    Render restaurantList and cart

--- components/context.js
+++ components/context.js
@@ -1,16 +1,16 @@
-import React from 'react';
+/* /context/AppContext.js */
 
+import React from "react";
+// create auth context with default value
+
+// set backup default for isAuthenticated if none is provided in Provider
 const AppContext = React.createContext(
-  {
-    isAuthenticated: true,
-    cart: {
-      items: [],
-      total:0
-    },
-    addItem: () => {},
-    user: false,
-    setUser: () => {}
-  }
-);
-
+    {isAuthenticated:true, 
+        cart: {items:[], 
+        total:0},
+        addItem:()=>{},
+        removeItem:()=>{},
+        user:false, 
+        setUser:()=>{}
+    });
 export default AppContext;
